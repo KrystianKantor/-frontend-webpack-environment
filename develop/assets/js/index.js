49bef74796b8c79a1add5a0fb598fc238268f118
Krystian Kantor <krykantor97@gmail.com>
2023-03-27 10:21:49 +0200

    first commit

---
+++ develop/assets/js/index.js
@@ -0,0 +1,8 @@
+// import "./header-scroll-behavior";
+// import "./mobileMenu";
+
+import "bootstrap/scss/bootstrap.scss";
+
+// import "../../../public/images/background.jpg";
+
+import "../scss/style.scss";
